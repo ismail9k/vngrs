a3b6a022f0cd09c121012e4121cf85e884a37300
Abdelrahman Ismail <dev@ismail9k.com>
2021-01-09 11:22:38 +0300

    chore: update config files

--- rollup.config.js
+++ rollup.config.js
@@ -18,9 +18,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'my-element.js',
+  input: 'my-app.js',
   output: {
-    file: 'my-element.bundled.js',
+    file: 'my-app.bundled.js',
     format: 'esm',
   },
   onwarn(warning) {
